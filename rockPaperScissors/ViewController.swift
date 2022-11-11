4a5559d55f2baa95dd481f1a6a6b3a0b966f4279
Sammi Sprague <samantha.sprague23@d155.org>
2022-11-11 07:27:10 -0600

    Initial Commit

---
+++ rockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rockPaperScissors
+//
+//  Created by SAMANTHA SPRAGUE on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
