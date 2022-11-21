0bca69e738dc1bd085326c01a6f2bd46a108de9b
Sammi Sprague <samantha.sprague23@d155.org>
2022-11-21 11:47:56 -0600

    added camera and created game

--- rockPaperScissors/ViewController2.swift
+++ rockPaperScissors/ViewController2.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
+class AppData{
+    static var move1: String!
+    static var move2: String!
+    static var move3: String!
+    static var pic1: UIImage!
+    static var pic2: UIImage!
+    static var pic3: UIImage!
+}
+
 class ViewController2: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var imageOutlet: UIImageView!
-    
+    @IBOutlet weak var image2Outlet: UIImageView!
+    @IBOutlet weak var image3Outlet: UIImageView!
+    var image: Int!
+    @IBOutlet weak var move1Outlet: UITextField!
+    @IBOutlet weak var move2Outlet: UITextField!
+    @IBOutlet weak var move3Outlet: UITextField!
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -25,15 +39,73 @@ class ViewController2: UIViewController, UINavigationControllerDelegate, UIImage
     @IBAction func move1Action(_ sender: Any) {
         imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
         present(imagePicker, animated: true, completion: nil)
+        image = 1
     }
+    
+    @IBAction func move2Action(_ sender: Any) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        image = 2
+    }
+    
+    
+    @IBAction func move3Action(_ sender: Any) {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        image = 3
+    }
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         picker.dismiss(animated: true){
-            self.imageOutlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            if self.image == 1{
+                self.imageOutlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+                AppData.pic1 = self.imageOutlet.image
+            }else if self.image == 2{
+                self.image2Outlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+                AppData.pic2 = self.image2Outlet.image
+            }else{
+                self.image3Outlet.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+                AppData.pic3 = self.image3Outlet.image
+            }
         }
     }
     
+    @IBAction func move1CameraAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        }else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
     
+    
+    @IBAction func move2CameraAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        }else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func move3CameraAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        }else{
+            imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func createAction(_ sender: Any) {
+        AppData.move1 = move1Outlet.text
+        AppData.move2 = move2Outlet.text
+        AppData.move3 = move3Outlet.text
+    }
     
     
     /*
---
+++ rockPaperScissors/ViewController3.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController3.swift
+//  rockPaperScissors
+//
+//  Created by SAMANTHA SPRAGUE on 11/21/22.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    
+    @IBOutlet weak var move1Outlet: UIImageView!
+    @IBOutlet weak var move2Outlet: UIImageView!
+    @IBOutlet weak var move3Outlet: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
